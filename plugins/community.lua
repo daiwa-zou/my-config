4b42f407a90f472201545bb64ca4855038190e80
Daiwa Zou <dzou@Daiwas-MacBook-Air.local>
2024-02-04 10:36:56 -0500

    Add community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,23 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- Colorscheme packs
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- Language packs
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.ruby" },
+  { import = "astrocommunity.pack.yaml" },
+  -- Motion packs
+  { import = "astrocommunity.motion.mini-basics" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.mini-bracketed" },
+  -- Terminal pack
+  { import = "astrocommunity.terminal-integration.flatten-nvim" },
 }
